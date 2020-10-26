a76fdcc898969345db26877e5ec7291bf0507751
gon-nexus <73172087+gon-nexus@users.noreply.github.com>
2020-10-26 13:49:16 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox', 'Firefox'],
     singleRun: false,
     restartOnFileChange: true
   });
