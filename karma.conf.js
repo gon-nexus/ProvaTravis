a3a8955be95e5836ce608d8c9d320c30c8a46725
gon-nexus <73172087+gon-nexus@users.noreply.github.com>
2020-10-27 09:30:32 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const path = require('path');
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
